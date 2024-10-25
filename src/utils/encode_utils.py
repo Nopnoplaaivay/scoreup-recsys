9881e56cf3737b68eeec8f2a13bdb02a4952039b
KhangVinh <vinhkhang378@gmail.com>
2024-10-25 09:22:06 +0700

    Encode utils

---
+++ src/utils/encode_utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from gensim.models import Word2Vec
+
+class EncodeQuestionsUtils:
+    def __init__(self) -> None:
+        self.label_encoder = LabelEncoder()
+        self.scaler = StandardScaler()
+
+    def get_concept_embedding(self, w2v_model, concept):
+        return w2v_model.wv[concept] if concept in w2v_model.wv else np.zeros(10)
+
+    def encode(self, df=None):
+        concepts = df['concept'].apply(lambda x: x.split())
+
+        w2v_model = Word2Vec(sentences=concepts, vector_size=10, window=3, min_count=1, sg=1)  
+        w2v_model.train(concepts, total_examples=w2v_model.corpus_count, epochs=10)
+
+        df['concept_embedding'] = df['concept'].apply(lambda x: self.get_concept_embedding(w2v_model, x))
+        concept_embedding_df = pd.DataFrame(df['concept_embedding'].tolist(), index=df.index)
+        df = pd.concat([df, concept_embedding_df], axis=1)
+        df.drop(columns=['concept_embedding'], inplace=True)
+        df.columns = df.columns.astype(str)
+
+        concept_embedding_df.columns = concept_embedding_df.columns.astype(str)
+        embedding_columns = concept_embedding_df.columns
+
+        df['chapter_encoded'] = self.label_encoder.fit_transform(df['chapter'])
+        df['difficulty_scaled'] = self.scaler.fit_transform(df[['difficulty']])
+
+        X = df[['chapter_encoded', 'difficulty_scaled'] + list(embedding_columns)] 
+        return X
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from src.db.questions import QuestionsCollection
 from src.models.clustering import QuestionClustering    
+from src.utils.encode_utils import EncodeQuestionsUtils
 
 questions_collection = QuestionsCollection()
 questions = questions_collection.fetch_all_questions()
-print(type(questions))
 
 data = questions_collection.preprocess_questions(raw_questions=questions)
-print(data)
+
+encode_utils = EncodeQuestionsUtils()
+print(encode_utils.encode(df=data))
